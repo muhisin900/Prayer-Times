21d9d75b60d5b10d612265d1ea5de0d87facc03d
Muhisin Abdihakim <muhisinabdihakim@gmail.com>
2024-07-15 13:07:28 -0500

    api

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,11 +1,16 @@
 import axios from "axios";
 
-//function to fetch prayer times from api
 export const fetchPrayerTimes = async (location) => {
-    const { latitude, longitude } = location;
+  const { city, country } = location;
+  const date = new Date().toISOString().split('T')[0]; // Get current date in YYYY-MM-DD format
 
+  try {
     const response = await axios.get(
-        `https://api.aladhan.com/v1/timings/${Date.now() / 1000}?latitude=${latitude}&longitude=${longitude}&method=2`
-      );
+      `http://api.aladhan.com/v1/timingsByCity/${date}?city=${encodeURIComponent(city)}&country=${encodeURIComponent(country)}&method=2`
+    );
     return response.data.data.timings;
-}
+  } catch (error) {
+    console.error('Error fetching prayer times:', error);
+    throw error;
+  }
+};
